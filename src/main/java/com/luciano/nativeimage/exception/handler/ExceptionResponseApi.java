fc55479ea4619c8e196d067e5c16c5f16d071bf2
luciano bigiotti <luciano.bigiotti@gmail.com>
2024-01-07 10:25:59 +0100

    Added response API

---
+++ src/main/java/com/luciano/nativeimage/exception/handler/ExceptionResponseApi.java
@@ -0,0 +1,20 @@
+package com.luciano.nativeimage.exception.handler;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ExceptionResponseApi {
+
+    private HttpStatus status;
+    private String message;
+    private String description;
+
+}
