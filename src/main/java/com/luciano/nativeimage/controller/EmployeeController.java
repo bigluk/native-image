783d3a58b3d1671338e2f2f1dea3274be7b8c186
luciano bigiotti <luciano.bigiotti@gmail.com>
2024-01-07 10:20:13 +0100

    Added controller code for insert new employee

---
+++ src/main/java/com/luciano/nativeimage/controller/EmployeeController.java
@@ -0,0 +1,68 @@
+package com.luciano.nativeimage.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.luciano.nativeimage.dto.EmployeeDto;
+import com.luciano.nativeimage.dto.EmployeeDto.Create;
+import com.luciano.nativeimage.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping(value = "/employees/v1/crud")
+public class EmployeeController {
+    
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @PostMapping(path = "/new-employee")
+    public ResponseEntity<EmployeeDto> addEmployee(@RequestBody @Validated(Create.class) EmployeeDto employee) throws Exception {
+
+        System.out.println(employee);
+
+        EmployeeDto employeeDtoSaved = employeeService.addNewEmployee(employee);
+
+        return new ResponseEntity<>(employeeDtoSaved, HttpStatus.CREATED);
+
+    }
+
+
+
+    @GetMapping(value = "/{employeeId}")
+    public Object getEmployee(@RequestParam String param) {
+        
+        // call some service   
+        return new Object();
+    
+    }
+    
+
+
+    @PutMapping(value = "/{employeeId}")
+    public Object updateEmployee(@PathVariable String id, @RequestBody Object entity) {
+        //TODO: process PUT request
+        
+        return entity;
+    }
+
+
+
+    @DeleteMapping(value = "/{employeeId}")
+    public Object deleteEmployee () {
+        return null;
+
+    }
+
+
+}
