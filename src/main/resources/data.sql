22750ba3fd777045b3ec35a025fde07d9f4885de
luciano bigiotti <luciano.bigiotti@gmail.com>
2024-01-08 11:09:05 +0100

    Added new employee to insert at start time

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO employee (name, surname, identification_number, department) VALUES ('Rino', 'Campa', 123, 'IT');
 INSERT INTO employee (name, surname, identification_number, department) VALUES ('Rollo', 'Ape', 234, 'Finance');
+INSERT INTO employee (name, surname, identification_number, department) VALUES ('Ketto', 'Whis', 345, 'Finance');
